0dba4139300150565d2a194bc2c085c899e7ee76
pawello2222 <pawello2222@gmail.com>
2017-05-20 00:44:47 +0200

    Added data loader

--- DataLoader.R
+++ DataLoader.R
@@ -1,9 +1,39 @@
 library(readr)
 
-loadData <- function()
+loadData <- function(base_dir)
 {
-  contents_of_file <- read_file("./Data/Space/Test")
-  row <- c(contents_of_file, "Space")
-  
-  #should return data as matrix with headers
+  data_frame <- loadCategory(base_dir, "Cryptography")
+  data_frame <- rbind(data_frame,loadCategory(base_dir, "Electronics"))
+  data_frame <- rbind(data_frame,loadCategory(base_dir, "Medicine"))
+  data_frame <- rbind(data_frame,loadCategory(base_dir, "Space"))
 }
+
+loadCategory <- function(base_dir, category_name)
+{
+  current_dir <- paste(base_dir, 'Data/', category_name, "/", sep="")
+  file_names=as.list(dir(path = current_dir,pattern="*", include.dirs = FALSE))
+  
+  data_frame <- NULL
+  count <- 100 #length(file_names)
+  
+  for(i in 1:count)
+  {
+    path <- paste(current_dir, file_names[[i]], sep="")
+    contents_of_file <- read_file(path)
+    
+    if (i==1)
+    {
+      data_frame <- data.frame(contents_of_file, category_name, stringsAsFactors=FALSE)
+    }
+    else
+    {
+      data_frame <- rbind(data_frame,c(contents_of_file, category_name))
+    }
+  }
+  
+  colnames(data_frame)[1] <- "Text"
+  colnames(data_frame)[2] <- "Category"
+  
+  return(data_frame)
+}
+
--- Main.R
+++ Main.R
@@ -1,9 +1,28 @@
 library(RTextTools)
+source("DataLoader.R")
 
-processData <- function(data)
+#TODO: is base_dir necessary?
+processData <- function(base_dir)
 {
-  dataMatrix <- create_matrix(data["Text"])
+  data <- loadData(base_dir)
   
-  #container <- create_container(dataMatrix, data$Category, trainSize=1:11, virgin=FALSE)
-  #model <- train_model(container, "SVM", kernel="linear", cost=1)
+  dataMatrix <- create_matrix(data["Text"], language="english", 
+                              minWordLength=3, maxWordLength=Inf, 
+                              removeNumbers=TRUE, removePunctuation=TRUE, 
+                              removeSparseTerms=0, removeStopwords=TRUE,
+                              stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE)
+  
+  container <- create_container(dataMatrix, data$Category, trainSize=1:100, virgin=FALSE)
+  model <- train_model(container, "SVM")
+  
+  predictionData <- list("electronics", "space ship blue air planet", "health")
+  predictionMatrix <- create_matrix(predictionData, originalMatrix=dataMatrix)
+  
+  predictionSize = length(predictionData)
+  predictionContainer <- create_container(predictionMatrix, labels=rep(0,predictionSize), 
+                                          testSize=1:predictionSize, virgin=FALSE)
+  
+  results <- classify_model(predictionContainer, model)
 }
+
+print(processData("~/Documents/Projekty/MOW/"))
